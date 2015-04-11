93282e663a03c2a62fcf9731db3d152b3d2c32c7
Cade Gillem <cpgillem@gmail.com>
2015-04-11 16:19:10 -0400

    Add MD+CSS processing test to test module.

--- test_publisher.py
+++ test_publisher.py
@@ -1,16 +1,26 @@
 import publisher
 
+test_pdf_filename = "test/test.pdf"
+test_css_filename = "test/test.css"
+test_md_filename = "test/test.md"
+test_html_filename = "test/test.html"
+
+test_md = "# Test heading\n\n- test item 1\n- test item 2"
+
 def from_html_file():
-    source_file = "~/Projects/markdown-publisher/source_test.md"
-    print publisher.get_html_from_file(source_file)
+    print publisher.md_to_html(publisher.from_file(test_md_filename))
 
-def from_html():
-    test_source = "# Test heading\n\n- test item 1\n- test item 2"
-    print publisher.get_html(test_source)
+def md_to_html():
+    print publisher.md_to_html(test_source)
 
-def from_html_to_pdf():
-    test_html = publisher.get_html(publisher.get_text_from_file("README.md"))
-    test_pdf_filename = "test.pdf"
-    print publisher.write_html_to_pdf(test_html, test_pdf_filename)
+def md_and_css_to_html():
+    html_source = publisher.md_and_css_to_html(publisher.from_file(test_md_filename),
+        publisher.from_file(test_css_filename))
+    print html_source
+    publisher.to_file(html_source, test_html_filename)
 
-from_html_to_pdf()
+def from_md_file_to_pdf_file():
+    test_html = publisher.md_to_html(publisher.from_file("README.md"))
+    print publisher.html_to_pdf_file(test_html, test_pdf_filename, [test_css_filename])
+
+md_and_css_to_html()
